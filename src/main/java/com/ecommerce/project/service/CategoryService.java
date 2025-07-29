00eda1dfe9a7fdc0fe8694b5c2290c34669f0d3a
MohitSainiDev <mht4943@gmail.com>
2025-07-29 19:49:28 +0530

    Added updateCategory controller and also added proper responseStatus codes using ResponseEntity Class

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,4 +12,5 @@ public interface CategoryService {
     List<Category> getAllCategories();
     String createCategory(Category category);
     String deleteCategory(Long CategoryId);
+    String updateCategory(Category category , Long CategoryId);
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,14 +29,18 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public String deleteCategory(Long CategoryId) {
-        Category category = categories.stream().filter(c -> c.getCategoryId().equals(CategoryId)).findFirst().orElse(null);
-        if(category!=null)
-        {
-            categories.remove(category);
+        Category category = categories.stream().filter(c -> c.getCategoryId().equals(CategoryId)).findFirst().orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND , "Not Found"));
+        categories.remove(category);
             return "category with category id " + CategoryId + " deleted successfully";
-        }
-        else
-            return  "category with category id " + CategoryId + " not found";
+
+
+    }
+
+    @Override
+    public String updateCategory(Category category , Long CategoryId) {
+        Category category1 = categories.stream().filter(c->c.getCategoryId().equals(CategoryId)).findFirst().orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND , "Not Found"));
+        category1.setCategoryName(category.getCategoryName());
+        return "category with category id " + CategoryId + " updated successfully";
     }
 
 }
